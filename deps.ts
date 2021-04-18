bf6d61fd8108e3b615c60f4b476ef349650a8a8d
木杉 <zhmushan@qq.com>
2021-04-18 20:57:06 +0800

    upgrade abc to 1.3.1

--- deps.ts
+++ deps.ts
@@ -1,10 +1,10 @@
 export {
   Application,
   NotFoundException,
-} from "https://deno.land/x/abc@v1.3.0/mod.ts";
-export { cors } from "https://deno.land/x/abc@v1.3.0/middleware/cors.ts";
-export { logger } from "https://deno.land/x/abc@v1.3.0/middleware/logger.ts";
-export { Header, MIME } from "https://deno.land/x/abc@v1.3.0/constants.ts";
+} from "https://deno.land/x/abc@v1.3.1/mod.ts";
+export { cors } from "https://deno.land/x/abc@v1.3.1/middleware/cors.ts";
+export { logger } from "https://deno.land/x/abc@v1.3.1/middleware/logger.ts";
+export { Header, MIME } from "https://deno.land/x/abc@v1.3.1/constants.ts";
 export * as path from "https://deno.land/std@0.90.0/path/mod.ts";
 export * as fs from "https://deno.land/std@0.90.0/fs/mod.ts";
 export * as flags from "https://deno.land/std@0.90.0/flags/mod.ts";
